b5ee6af0c6b97403fd4e35fef4757161190a04c1
rsenjo <senjorado@gmail.com>
2022-01-10 18:10:40 +0100

    ConflictCommit

--- dat/prosjek.py
+++ dat/prosjek.py
@@ -3,4 +3,5 @@ ocjena2 = int(input('Unesi drugu ocjenu: '))
 ocjena3 = int(input('Unesi treću ocjenu: '))
 ocjena4 = int(input('Unesi četvrtu ocjenu: '))
 prosjek = (ocjena1 + ocjena2 + ocjena3 + ocjena4)/4
+#Komentar1
 print('Prosjek ocjena:', prosjek)
