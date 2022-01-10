27e81489080b23e81e52bbbc6b2466a9c9d012cf
rsenjo <senjorado@gmail.com>
2022-01-10 18:03:28 +0100

    ProsjekCommit

---
+++ dat/prosjek.py
@@ -0,0 +1,6 @@
+ocjena1 = int(input('Unesi prvu ocjenu: '))
+ocjena2 = int(input('Unesi drugu ocjenu: '))
+ocjena3 = int(input('Unesi treću ocjenu: '))
+ocjena4 = int(input('Unesi četvrtu ocjenu: '))
+prosjek = (ocjena1 + ocjena2 + ocjena3 + ocjena4)/4
+print('Prosjek ocjena:', prosjek)
